78444bf97644aaf3512cbc6c44c42da41828c620
tonychg <tonychg7@pm.me>
2023-12-08 14:12:33 +0100

    refactor(downloader): move fake downloader from tests

--- src/downloader.rs
+++ src/downloader.rs
@@ -8,54 +8,54 @@ pub trait Downloader {
     fn download(&self, document: &Document) -> Result<Document>;
 }
 
+pub struct FakeDownloader {
+    directory: String,
+}
+
+impl FakeDownloader {
+    fn new(directory: &str) -> Self {
+        Self {
+            directory: directory.to_string(),
+        }
+    }
+}
+
+impl Downloader for FakeDownloader {
+    fn is_available(&self, document: &Document) -> Result<bool> {
+        Ok(document.url().starts_with("https://example.com"))
+    }
+
+    fn get_filename(&self, document: &Document) -> Result<Document> {
+        let mut document = document.clone();
+        document.set_filename(format!("{}.mp4", document.id()));
+        Ok(document)
+    }
+
+    fn download(&self, document: &Document) -> Result<Document> {
+        let mut document = self.get_filename(document)?;
+        document.set_local_path(format!(
+            "{}/{}",
+            self.directory,
+            document.filename().unwrap()
+        ));
+        Ok(document)
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    pub struct MockDownloader {
-        directory: String,
-    }
-
-    impl MockDownloader {
-        fn new(directory: &str) -> Self {
-            Self {
-                directory: directory.to_string(),
-            }
-        }
-    }
-
-    impl Downloader for MockDownloader {
-        fn is_available(&self, document: &Document) -> Result<bool> {
-            Ok(document.url().starts_with("https://example.com"))
-        }
-
-        fn get_filename(&self, document: &Document) -> Result<Document> {
-            let mut document = document.clone();
-            document.set_filename(format!("{}.mp4", document.id()));
-            Ok(document)
-        }
-
-        fn download(&self, document: &Document) -> Result<Document> {
-            let mut document = self.get_filename(document)?;
-            document.set_local_path(format!(
-                "{}/{}",
-                self.directory,
-                document.filename().unwrap()
-            ));
-            Ok(document)
-        }
-    }
-
     #[test]
     fn test_downloader_is_available_with_valid_document() {
-        let downloader = MockDownloader::new("/tmp");
+        let downloader = FakeDownloader::new("/tmp");
         let document = Document::new("https://example.com");
         assert_eq!(downloader.is_available(&document).unwrap(), true);
     }
 
     #[test]
     fn test_downloader_download_with_valid_document() {
-        let downloader = MockDownloader::new("/tmp");
+        let downloader = FakeDownloader::new("/tmp");
         let document = Document::new("http://example.com");
         let document = downloader.download(&document).unwrap();
         assert_eq!(
