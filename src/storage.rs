24d765ff11582459471176efbf84be4c76662219
tonychg <tonychg7@pm.me>
2023-12-08 14:11:55 +0100

    refactor(storage): move fake storage from tests

--- src/storage.rs
+++ src/storage.rs
@@ -7,50 +7,50 @@ pub trait Storage {
     fn upload(&self, document: &Document) -> Result<Save>;
 }
 
+pub struct FakeStorage {
+    directory: String,
+}
+
+impl FakeStorage {
+    fn new(directory: &str) -> Self {
+        Self {
+            directory: directory.to_string(),
+        }
+    }
+}
+
+impl Storage for FakeStorage {
+    fn exists(&self, document: &Document) -> Result<bool> {
+        match document.filename() {
+            Some(filename) => match filename.as_str() {
+                "existing" => Ok(true),
+                _ => Ok(false),
+            },
+            None => Ok(false),
+        }
+    }
+
+    fn upload(&self, document: &Document) -> Result<Save> {
+        let mut save = Save::new(&document.id());
+        save.set_url(format!("{}/{}", self.directory, save.id()));
+        Ok(save)
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    pub struct MockStorage {
-        directory: String,
-    }
-
-    impl MockStorage {
-        fn new(directory: &str) -> Self {
-            Self {
-                directory: directory.to_string(),
-            }
-        }
-    }
-
-    impl Storage for MockStorage {
-        fn exists(&self, document: &Document) -> Result<bool> {
-            match document.filename() {
-                Some(filename) => match filename.as_str() {
-                    "existing" => Ok(true),
-                    _ => Ok(false),
-                },
-                None => Ok(false),
-            }
-        }
-
-        fn upload(&self, document: &Document) -> Result<Save> {
-            let mut save = Save::new(&document.id());
-            save.set_url(format!("{}/{}", self.directory, save.id()));
-            Ok(save)
-        }
-    }
-
     #[test]
     fn test_storage_exists_with_not_existing_document() {
-        let storage = MockStorage::new("/tmp");
+        let storage = FakeStorage::new("/tmp");
         let document = Document::new("http://example.com");
         assert_eq!(storage.exists(&document).unwrap(), false);
     }
 
     #[test]
     fn test_storage_exists_with_existing_document() {
-        let storage = MockStorage::new("/tmp");
+        let storage = FakeStorage::new("/tmp");
         let mut document = Document::new("http://example.com");
         document.set_filename("existing".to_string());
         storage.upload(&document).unwrap();
@@ -59,7 +59,7 @@ mod tests {
 
     #[test]
     fn test_storage_upload_with_valid_document() {
-        let storage = MockStorage::new("/tmp");
+        let storage = FakeStorage::new("/tmp");
         let document = Document::new("http://example.com");
         let save = storage.upload(&document).unwrap();
         assert_eq!(save.document_id(), document.id());
