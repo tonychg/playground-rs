fc846b23465076520b80219e2b4454ae776c27ad
tonychg <tonychg7@pm.me>
2023-12-08 15:20:41 +0100

    test(document_service): test document_service with fake backend

--- src/service.rs
+++ src/service.rs
@@ -9,6 +9,8 @@ use crate::{
 pub type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 
 pub trait DocumentService {
+    fn get_document_by_id(&self, document_id: &str) -> Result<Option<Document>>;
+    fn get_document_by_url(&self, url: &str) -> Result<Option<Document>>;
     fn download(&mut self, url: &str) -> Result<Document>;
     fn store(&mut self, document_id: &str) -> Result<Save>;
 }
@@ -37,6 +39,14 @@ impl DocumentServiceFactory {
 }
 
 impl DocumentService for DocumentServiceFactory {
+    fn get_document_by_id(&self, document_id: &str) -> Result<Option<Document>> {
+        Ok(self.document_repository.find(document_id)?)
+    }
+
+    fn get_document_by_url(&self, url: &str) -> Result<Option<Document>> {
+        Ok(self.document_repository.find_by_url(url)?)
+    }
+
     fn download(&mut self, url: &str) -> Result<Document> {
         let document = self.document_repository.find_by_url(url)?;
         if document.is_some() {
@@ -61,12 +71,103 @@ impl DocumentService for DocumentServiceFactory {
         }
         let document = document.unwrap();
         for storage in self.storages.iter() {
-            if storage.exists(&document)? {
+            if !storage.exists(&document)? {
                 let save = storage.upload(&document)?;
                 self.save_repository.save(&save)?;
                 return Ok(save);
+            } else {
+                return Err("Document already stored".into());
             }
         }
         Err("No storage available".into())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{
+        downloader::FakeDownloader,
+        repository::{document::DocumentMemoryRepository, save::SaveMemoryRepository},
+        storage::FakeStorage,
+    };
+
+    use super::*;
+
+    fn create_fake_document_service() -> DocumentServiceFactory {
+        let document_repository = Box::new(DocumentMemoryRepository::new());
+        let save_repository = Box::new(SaveMemoryRepository::new());
+        let downloaders: Vec<Box<dyn Downloader>> = vec![Box::new(FakeDownloader::new("/tmp"))];
+        let storages: Vec<Box<dyn Storage>> = vec![Box::new(FakeStorage::new("/tmp"))];
+        DocumentServiceFactory::new(document_repository, save_repository, downloaders, storages)
+    }
+
+    #[test]
+    fn test_document_service_download_with_valid_url() {
+        let mut document_service = create_fake_document_service();
+        let document_downloaded = document_service.download("https://example.com").unwrap();
+        let document = document_service
+            .get_document_by_id(&document_downloaded.id())
+            .unwrap()
+            .unwrap();
+        assert_eq!(document.url(), "https://example.com");
+        assert_eq!(
+            document.filename(),
+            Some(format!("{}.mp4", document.id().to_string()))
+        );
+    }
+
+    #[test]
+    fn test_document_service_download_with_invalid_url() {
+        let mut document_service = create_fake_document_service();
+        let document_downloaded = document_service.download("https://example.org");
+        assert!(document_downloaded.is_err());
+    }
+
+    #[test]
+    fn test_document_service_download_with_already_existing_document() {
+        let mut document_service = create_fake_document_service();
+        document_service.download("https://example.com").unwrap();
+        let document_downloaded = document_service.download("https://example.com");
+        assert_eq!(
+            document_downloaded.unwrap_err().to_string(),
+            "Document already exists"
+        );
+    }
+
+    #[test]
+    fn test_document_service_get_document_by_url_with_valid_document() {
+        let mut document_service = create_fake_document_service();
+        let document_downloaded = document_service.download("https://example.com").unwrap();
+        let document = document_service
+            .get_document_by_url("https://example.com")
+            .unwrap()
+            .unwrap();
+        assert_eq!(document_downloaded.id(), document.id());
+        assert_eq!(document_downloaded.url(), document.url());
+    }
+
+    #[test]
+    fn test_document_service_store_with_valid_document() {
+        let mut document_service = create_fake_document_service();
+        let document_downloaded = document_service.download("https://example.com").unwrap();
+        let save = document_service.store(&document_downloaded.id()).unwrap();
+        assert_eq!(save.document_id(), document_downloaded.id());
+    }
+
+    #[test]
+    fn test_document_service_store_with_invalid_document() {
+        let mut document_service = create_fake_document_service();
+        let save = document_service.store("invalid_document_id");
+        assert_eq!(save.unwrap_err().to_string(), "Document not found");
+    }
+
+    #[test]
+    fn test_document_service_store_with_already_stored_document() {
+        let mut document_service = create_fake_document_service();
+        let document_downloaded = document_service
+            .download("https://example.com/existing")
+            .unwrap();
+        let save = document_service.store(&document_downloaded.id());
+        assert_eq!(save.unwrap_err().to_string(), "Document already stored");
     }
 }
